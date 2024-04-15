2969e3c696f5d6875f31c780825a9f7921cc8fb0
Hnin <hwutyee@gmail.com>
2024-04-15 21:33:24 +0800

    Updated Post Man

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/wutyee', (req, res) => {
+    res.send('Hello Wut Yee!')
+  })
 app.post('/world', (req, res) => {
     res.send('Hello World POST!')
   })
